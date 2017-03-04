1d66c76bcf4f7814ac421bcc083fbf6291cc512f
Ankit1217103 <aagupta2016@gmail.com>
2017-03-05 00:02:38 +0530

    Update plot4.r
    Adding Comments

--- plot4.r
+++ plot4.r
@@ -3,23 +3,21 @@ householdFile <- "household_power_consumption.txt"
 plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
 
 ## Subsetting the data
-stopdate <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
-startdate <-strptime(paste(stopdate$Date, stopdate$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
-stopdate <- cbind(startdate, stopdate)
+SetDate <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(SetDate$Date, SetDate$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+SetDate <- cbind(SetTime, SetDate)
 
 
 ## Plot 4
+png("plot4.png", width = 480, height = 480)
 labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
 columnlines <- c("black","red","blue")
 par(mfrow=c(2,2))
-plot(stopdate$startdate, stopdate$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
-plot(stopdate$startdate, stopdate$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
-plot(stopdate$startdate, stopdate$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-lines(stopdate$startdate, stopdate$Sub_metering_2, type="l", col="red")
-lines(stopdate$startdate, stopdate$Sub_metering_3, type="l", col="blue")
+plot(SetDate$SetTime, SetDate$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
+plot(SetDate$SetTime, SetDate$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
+plot(SetDate$SetTime, SetDate$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(SetDate$SetTime, SetDate$Sub_metering_2, type="l", col="red")
+lines(SetDate$SetTime, SetDate$Sub_metering_3, type="l", col="blue")
 legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
-plot(stopdate$startdate, stopdate$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
-
-## Saving to file
-png("plot4.png", width = 480, height = 480)
+plot(SetDate$SetTime, SetDate$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
