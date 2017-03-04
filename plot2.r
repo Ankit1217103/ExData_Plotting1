259c15860782473147aa5470595b811f5f7cb74b
Ankit1217103 <aagupta2016@gmail.com>
2017-03-05 00:01:20 +0530

    Update plot2.r
    Adding Comments

--- plot2.r
+++ plot2.r
@@ -3,13 +3,11 @@ householdFile <- "household_power_consumption.txt"
 plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
 
 ## Subsetting the data
-stopdate <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
-startdate <-strptime(paste(stopdate$Date, stopdate$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
-stopdate <- cbind(startdate, stopdate)
+SetDate <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(SetDate$Date, SetDate$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+SetDate <- cbind(SetTime, SetDate)
 
 ## Plot 2
-plot(stopdate$startdate, stopdate$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
-
-## Saving to file
-png("plot1.png", width = 480, height = 480)
+png("plot2.png", width = 480, height = 480)
+plot(SetDate$SetTime, SetDate$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
